010f5acaf6a3a660da9e39218dd6e0cda94e9431
Ludovic Jacquelin <ludovic.jacquelin@inxio.net>
2018-03-15 18:05:20 +0100

    maj

--- xio/core/app/app.py
+++ xio/core/app/app.py
@@ -98,7 +98,7 @@ def handleCache(func):
     def _(res,req):
         # need explicit configuration => about.ttl
 
-        if req.GET and req.path and req.path.startswith('www'):
+        if req.GET and req.path:
             
             cacheservice = res.get('services/cache') 
             if cacheservice:
