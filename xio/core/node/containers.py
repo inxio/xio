7304f07afedf10ccd15fad554017c29e5cf8430e
Ludovic Jacquelin <ludovic.jacquelin@inxio.net>
2018-03-19 13:28:26 +0100

    maj

---
+++ xio/core/node/containers.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*--
+
+import sys
+import os
+from pprint import pprint
+import datetime
+import yaml
+
+import xio 
+
+
+
+
+class Containers:
+
+    def __init__(self,node):
+        self.node = node
+        self.docker = node.service('docker').content  # skip resource wrapper
+        self.ipfs = node.service('ipfs')
+        self.db = node.service('db')
+
+        # warning, service may be unavalable yet (required full app/init or app/start)
+
+    def get(self,uri):
+
+        print ('get container', uri)
+
+        if uri.startswith('/'):
+            container = Container(self,directory=uri)
+            return container
+
+
+
+    def deliver(self,uri):
+
+        container = self.get(uri)
+        container.run()
+
+        if container.endpoint:
+            self.node.register(container.endpoint)
+
+
+    def sync(self):
+
+        running_endpoints = []
+        for container in self.docker.containers():
+            name = container.data['name']
+            http_endpoint = container.data['endpoints'].get('http')
+            if http_endpoint:
+                running_endpoints.append(http_endpoint)
+
+        for endpoint in running_endpoints:
+            try:
+                print ('*** register docker container', endpoint)
+                self.node.peers.register( endpoint )
+            except Exception as err:
+                #import traceback
+                #traceback.print_exc()
+                print ('dockersync error',err) 
+        """        
+        print '=== peers'
+        for peer in node.peers.select():
+            if peer.endpoint and '127.0.0.1' in peer.endpoint:
+                print peer.name, peer.endpoint
+        """
+
+    def select(self):
+        return self.docker.containers('xio')   
+        
+
+    def images(self):
+        return self.docker.images('xio') 
+            
+
+    """
+    running_endpoints = []
+    for container in containers:
+        name = container.get('Names')[0]
+        running = bool(container.get('State'))
+        if 'inxio' in name:
+            from pprint import pprint 
+            #pprint(container)
+
+            for p in container.get('Ports'):
+                if p.get('PrivatePort')==8080:
+                    port = p.get('PublicPort')
+                    #print '*****', name, running,port
+                    running_endpoints.append('http://127.0.0.1:%s' % port)
+                elif p.get('PrivatePort')==8484:
+                    port = p.get('PublicPort')
+                    #print '*****', name, running,port
+                    running_endpoints.append('ws://127.0.0.1:%s' % port)
+
+    print '*** running_endpoints', running_endpoints
+
+    for peer in node.peers.select():
+        if not peer.endpoint:
+            running_endpoints.remove(peer.endpoint)
+        elif '127.0.0.1' in peer.endpoint: 
+            try:
+                if peer.endpoint not in running_endpoints:
+                    print '*** remove', peer.endpoint
+                    node.peers.unregister( peer.peerid )
+                else:
+                    running_endpoints.remove(peer.endpoint)
+            except Exception,err:
+                print 'dockersync error',err    
+
+    for endpoint in running_endpoints:
+        try:
+            print '*** add', endpoint
+            node.peers.register( endpoint )
+        except Exception,err:
+            #import traceback
+            #traceback.print_exc()
+            print 'dockersync error',err    
+        """
+
+
+
+
+class Container:
+
+    def __init__(self,containers,directory=None,data=None):
+
+        self.containers = containers
+        self.docker = containers.docker # skip resource wrapper
+        self.directory = directory
+
+        if not data:
+            about_filepath = directory+'/about.yml'
+            with open(about_filepath) as f:
+                data = yaml.load(f)
+
+        self.data = data
+
+        self.name = data.get('name')
+        nfo = self.name.split(':')
+        nfo.pop(0)  # strip xrn:
+
+        self.iname = '/'.join(nfo)
+        self.cname = '_'.join(nfo)
+
+        self.image = self.docker.image(name=self.iname)
+        self.container = self.docker.container(name=self.cname)
+        self.endpoint = None
+
+
+
+    def __repr__(self):
+        return 'CONTAINER %s %s' % (self.iname, self.cname)
+
+    def start(self):
+        print('STARTING SERVICE',self.name)
+        if not self.container:
+            self.run()
+
+    def about(self):
+        about = self.data
+        about.update({
+            'image': self.image.about() if self.image else {'name': self.iname},
+            'container': self.container.about() if self.container else {'name': self.cname},
+        })
+        return about
+
+
+    def build(self):
+        #https://docker-py.readthedocs.io/en/stable/images.html
+        #self.image.build()
+        dockerfile = self.directory+'/Dockerfile'
+        if not os.path.isfile(dockerfile):
+            dockerfile = "from xio/app"
+
+        self.docker.build(name=self.iname,directory=self.directory,dockerfile=dockerfile)
+        self.image = self.docker.image(name=self.iname)
+        assert self.image
+        pprint(self.image.about())
+        
+
+    def run(self):
+
+        cport = 80
+
+        info = {
+            'name': self.cname,
+            'image': self.iname,
+            'ports': {
+                0: cport,
+            },
+            'volumes': {
+                '/apps/xio': '/apps/xio',
+            }
+        }
+        self.container = self.docker.run(**info)
+
+        portmapping = self.about().get('container').get('port') # receive {32776: 8080}
+        for k,v in portmapping.items():
+            if v==cport:
+                self.endpoint = 'http://127.0.0.1:%s' % k
+
+
+
+
+
+
+    
+
+
+
+   
---
+++ xio/core/node/lib/docker/client.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*--
+from __future__ import absolute_import
+import sys
+import os
+from pprint import pprint
+import datetime
+
+
+
+class Docker:
+
+    def __init__(self):
+
+        import docker
+        
+        #self.docker = docker.Client(base_url='unix://var/run/docker.sock',version='auto') #1.23
+        self.docker = docker.from_env()
+        import xio
+        self.host = xio.env('app_host') or '127.0.0.1'
+        
+    def build(self,name=None,directory=None,dockerfile=None,**kwargs):
+        """
+        # https://docker-py.readthedocs.io/en/stable/images.html
+        # https://github.com/docker/docker-py/issues/1400
+        from io import BytesIO
+        import docker
+        dockerfile = 'FROM alpine:3.6'
+        f = BytesIO(dockerfile.encode('utf-8'))
+        #image = self.docker.images.build(fileobj=f, tag='test/py', nocache=True, rm=True) # rm=True, 
+
+        # alternative avec logs : http://docker-py.readthedocs.io/en/stable/api.html#module-docker.api.build
+        """
+        from docker import APIClient
+        from io import BytesIO
+
+        cli = APIClient(base_url='unix://var/run/docker.sock',version='auto')
+
+        kwargs = dict(
+            path=directory,
+            rm=True, 
+            tag=name, 
+            decode=True
+        )
+
+        if not dockerfile.startswith('/'):
+            f = BytesIO(dockerfile.encode('utf-8'))
+            kwargs['fileobj'] = f
+        else:
+            kwargs['dockerfile'] = dockerfile
+
+
+        # bug IOError: Can not access file in context => https://github.com/docker/docker-py/issues/1841
+        for line in cli.build(**kwargs): # squash=True ??? "squash is only supported with experimental mode
+            print( line.get('stream','').strip() )
+        
+        return self.image(name)
+
+    def run(self,image,**info):
+        # https://docker-py.readthedocs.io/en/stable/containers.html#
+        assert image
+        print ('run', image,info)
+        iname = image
+        cname = info.get('name',iname.replace('/','-'))
+        ports = {}
+        for key,val in info.get('ports',{}).items():
+            ports[val] = key
+        
+        """
+        volumes (dict or list) –
+        A dictionary to configure volumes mounted inside the container. The key is either the host path or a volume name, and the value is a dictionary with the keys:
+
+        bind The path to mount the volume inside the container
+        mode Either rw to mount the volume read/write, or ro to mount it read-only.
+        For example:
+
+        {'/home/user1/': {'bind': '/mnt/vol2', 'mode': 'rw'},
+         '/var/www': {'bind': '/mnt/vol1', 'mode': 'ro'}}
+
+        """
+        volumes = info.get('volumes',{})
+        for src,dst in volumes.items():
+            volumes[src] = {'bind': dst, 'mode': 'rw'} # 'mode': 'ro'
+
+        # stop and remove previous container
+        c = self.container(name=cname)
+        if c:
+            print ('... stop and remove',cname)
+            c.stop()
+            c.remove()
+
+        kwargs = dict(name=cname,detach=True,ports=ports,volumes=volumes)
+        print ('... RUN iname', kwargs)
+        c = self.docker.containers.run(iname,**kwargs)
+        cid = c.attrs.get('Id')
+        return self.container(id=cid)
+
+
+
+    def images(self,pattern=None):
+        images = self.docker.images.list(all=True)
+        result = []     
+        for i in images:
+            image = Image(self,i)
+            if image.name and ( not pattern or pattern in image.name):
+                result.append(image)
+        return result                    
+              
+    def image(self,name=None,id=None):
+        images = self.images()
+        for image in images:
+            if id and image.id==id:
+                return image
+
+            if name and image.name==name:
+                return image
+
+    def containers(self,pattern=None,image=None):
+        containers = self.docker.containers.list(all=True)   
+        result = []
+        for c in containers:
+            container = Container(self,c)
+            if container.name and ( not pattern or pattern in c.name):
+                if not image or image==container.image:
+                    result.append(container)
+        return result  
+
+    def container(self,name=None,id=None):
+        containers = self.containers()
+        for container in containers:
+            if id and container.id==id:
+                return container
+            print ('...', name, container.name)
+            if name and container.name==name:
+                return container
+
+  
+
+
+class Image:
+
+    def __init__(self,docker,image):
+        self.docker = docker
+        self.image = image
+        self.raw = image.attrs
+        self.id = image.attrs.get('Id')
+
+        repo = image.attrs.get('RepoTags')
+        self.name,self.version = repo[0].split(':') if repo else ('','')
+        self.name = self.name.strip()
+
+        self.size = int(image.attrs.get('Size')/1000000)
+        self._about = {
+            'id': self.id,
+            'name': self.name,
+            'version': self.version,
+            'created': image.attrs.get('Created'),
+            'size': self.size,        
+        }
+        
+
+    def __repr__(self):
+        return 'DOCKER IMAGE #%s : %s %s:%s (created %s, size %s Mb)' % (self.id,self.name,self.version,self.version,self._about.get('created'),self._about.get('size'))
+
+    def about(self):
+        return self._about
+
+    def containers(self):
+        return self.docker.containers(image=self.image.id)
+
+
+
+
+class Container:
+
+    def __init__(self,docker,container):
+        self.docker = docker
+        self.container = container
+        self.raw = container.attrs
+        self.name = container.attrs.get('Name')[1:]
+        self.id = container.attrs.get('Id')
+        #self.image = self.docker.image(id=container.attrs.get('Image'))
+        self.image = container.attrs.get('Image')
+
+        info = self.raw
+        #created = datetime.datetime.fromtimestamp(info.get('Created')).strftime('%Y-%m-%d %H:%M')  
+        http_endpoint = ws_endpoint = None 
+
+        """
+        u'Ports': {
+            u'80/tcp': None,
+            u'81/tcp': [
+                {u'HostIp': u'0.0.0.0',
+                u'HostPort': u'7501'}
+            ],
+             u'84/tcp': None
+        },
+        """
+        ports = info.get('NetworkSettings',{}).get('Ports',{})
+        self.ports = {}
+        for k,v in ports.items():
+            if v:
+                p = v[0].get('HostPort')
+                if p:
+                    self.ports[int(p)] = int( k.split('/')[0] )
+
+        self.status = info.get('State').get('Running')==True
+        self.running = info.get('State').get('Running')==True
+        self._about = {
+            'id': self.id,
+            'name': self.name,
+            'created': info.get('Created'),
+            'status': self.status,
+            'image': self.image,
+            'port': self.ports
+        }
+        
+    def __repr__(self):
+        return 'DOCKER CONTAINER #%s : %s (%s)' % (self.id,self.name,self._about.get('status'))
+
+    def about(self):
+        return self._about
+
+    def stop(self):
+        return self.container.stop()
+
+    def remove(self):
+        return self.container.remove()
+
+
+   
